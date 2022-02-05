d04029307fe4ac046779f637fa175cecbaafb059
Katsutaka Suzuki <katsutakasuzuki0926@gmail.com>
2022-02-05 16:19:50 +0000

    added test codes

---
+++ test_geo.py
@@ -0,0 +1,25 @@
+from floodsystem.geo import rivers_with_station, stations_by_river, rivers_by_station_number, stations_within_radius
+from floodsystem.stationdata import build_station_list
+
+stations = build_station_list()
+
+def test_rivers_with_station():
+    a = rivers_with_station(stations)
+    assert type(a) == list
+    assert len(a) > 0
+
+
+def test_stations_by_river():
+    a = stations_by_river(stations)
+    assert type(a) == dict
+
+def test_rivers_by_station_number():
+    a = rivers_by_station_number(stations, 10)
+    assert type(a) == list
+    if len(a) > 10:
+        assert a[-1][1] == a[-2][1]
+
+
+
+
+

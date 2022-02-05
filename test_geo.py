44e16ba95af3f39903fd4812a9f2a80451dda5ad
Katsutaka Suzuki <katsutakasuzuki0926@gmail.com>
2022-02-05 20:54:38 +0000

    how is this

--- test_geo.py
+++ test_geo.py
@@ -1,5 +1,6 @@
-from floodsystem.geo import rivers_with_station, stations_by_river, rivers_by_station_number, stations_within_radius
+from floodsystem.geo import rivers_with_station, stations_by_river, rivers_by_station_number, stations_within_radius, stations_within_radius
 from floodsystem.stationdata import build_station_list
+from floodsystem.station import MonitoringStation
 
 stations = build_station_list()
 
@@ -17,10 +18,20 @@ def test_rivers_by_station_number():
     # maybe this has a problem?
     a = rivers_by_station_number(stations, 10)
     assert type(a) == list
-    if len(a) > 10:
-        assert a[-1][1] == a[-2][1]
-    else:
-        pass
+
+def test_stations_within_radius():
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "some station"
+    coord = (-2.0, 4.0)
+    trange = (-2.3, 3.4445)
+    river = "River X"
+    town = "My Town"
+    s = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
+    stations = [s]
+    n = stations_within_radius(stations, (0, 0), 30)
+    assert n == 1
+
 
 
 

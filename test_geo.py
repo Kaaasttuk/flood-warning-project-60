20efef277ba5da8449ec7e3fe2eeb6b2dfadabee
Katsutaka Suzuki <katsutakasuzuki0926@gmail.com>
2022-02-05 20:47:17 +0000

    how is this

--- test_geo.py
+++ test_geo.py
@@ -14,12 +14,14 @@ def test_stations_by_river():
     assert type(a) == dict
 
 def test_rivers_by_station_number():
+    # maybe this has a problem?
     a = rivers_by_station_number(stations, 10)
     assert type(a) == list
     if len(a) > 10:
         assert a[-1][1] == a[-2][1]
     else:
         pass
+
 
 
 

5975dbb0c7211c431e5b599684e7e470113dd0ae
Jingtong Chen <jc2322@cam.ac.uk>
2022-02-27 00:40:25 +0000

    updated Task2C

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -12,6 +12,19 @@ def stations_level_over_threshold(stations, tol):
                 stations_level_over_threshold.insert(0, (station.name, station.relative_water_level()))
             else:
                 pass
-        stations_level_over_threshold=sorted_by_key(stations_level_over_threshold, 1)
-        stations_level_over_threshold.reverse()
+    stations_level_over_threshold=sorted_by_key(stations_level_over_threshold, 1)
+    stations_level_over_threshold.reverse()
+
     return stations_level_over_threshold
+
+
+def stations_highest_rel_level(stations, N):
+    stations_highest_rel_level=[]
+    for station in stations:
+        if station.relative_water_level() == None:
+            pass
+        else:
+            stations_highest_rel_level.insert(0, (station.name, station.relative_water_level()))
+    stations_highest_rel_level=sorted_by_key(stations_highest_rel_level, 1)
+    stations_highest_rel_level.reverse()
+    stations_highest_rel_level=stations_highest_rel_level[0:N-1]

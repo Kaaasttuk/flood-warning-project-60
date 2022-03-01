96b72f99fb2b86aaacb680829a3eaa5cc57fbf6c
Jingtong Chen <jc2322@cam.ac.uk>
2022-03-01 21:57:03 +0000

    updated test flood

--- test_flood.py
+++ test_flood.py
@@ -5,9 +5,18 @@ from floodsystem.station import MonitoringStation
 stations = build_station_list()
 
 def test_stations_level_over_threshold():
-    a = stations_level_over_threshold(stations,1000)
-    assert type(a) == list
-    assert len(a) == 0
+
+    s_id = "random station id"
+    m_id = "random measure id"
+    label = "Cambridge Station"
+    coord = (1.0, 1.0)
+    trange = (-2.0, 3.0)
+    river = "Cambridge River"
+    town = "Cambridge Town"
+    Cambridge_Station = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
+    station = [Cambridge_Station]
+    list = stations_level_over_threshold (station, 0)
+    assert len(list) == 1
 
 def test_stations_highest_rel_level():
     a = stations_highest_rel_level(stations,0)

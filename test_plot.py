260109304ba0f7310fee9688002d1aae0a46acb8
Jingtong Chen <jc2322@cam.ac.uk>
2022-03-01 02:01:34 +0000

    tests for plots

---
+++ test_plot.py
@@ -0,0 +1,11 @@
+from floodsystem.plot import plot_water_levels, plot_water_level_with_fit
+import matplotlib.pyplot as plt
+
+def test_plot_water_levels():
+
+
+
+
+
+
+def test_plot_water_level_with_fit():

f38c311d6ed8ca5227c537ff7b1ae926e91a2bde
Jingtong Chen <jc2322@cam.ac.uk>
2022-03-01 01:44:52 +0000

    tests for flood.py

---
+++ test_flood.py
@@ -0,0 +1,15 @@
+from floodsystem.flood import stations_level_over_threshold, stations_highest_rel_level
+from floodsystem.stationdata import build_station_list
+from floodsystem.station import MonitoringStation
+
+stations = build_station_list()
+
+def stations_level_over_threshold():
+    a = stations_level_over_threshold(stations,10)
+    assert type(a) == list
+    assert len(a) == 0
+
+def tstations_highest_rel_level():
+    a = stations_highest_rel_level(stations,5)
+    assert type(a) == list
+    assert len(a) == 5
